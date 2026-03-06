54389b0dee8c31518547bca8a7732b4d2de0ee17
XaviDron12 <xavierbarcelona2019@gmail.com>
2026-03-06 13:07:35 +0100

    Componente valoracion-button-component creado

---
+++ src/app/features/feedback/valoracion/ui/valoracion-button-component/valoracion-button-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-valoracion-button-component',
+  imports: [],
+  templateUrl: './valoracion-button-component.html',
+  styleUrl: './valoracion-button-component.css',
+})
+export class ValoracionButtonComponent {
+
+}
