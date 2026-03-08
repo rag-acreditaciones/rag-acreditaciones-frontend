856793b65cee7bdadd209f31ae968d159ecb0e75
yordaw <yorradgeo@alu.edu.gva.es>
2026-03-08 12:01:57 +0100

    componente creado y rutas hechas

---
+++ src/app/features/documentos/documento-upload-component/documento-upload.component.ts
@@ -0,0 +1,11 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-documento-upload',
+  standalone: true,
+  templateUrl: './documento-upload.component.html',
+  styleUrls: ['./documento-upload.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DocumentoUploadComponent {
+}
