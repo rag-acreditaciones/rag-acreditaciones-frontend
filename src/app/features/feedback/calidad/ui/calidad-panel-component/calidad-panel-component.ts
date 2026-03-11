edb4437b8ceda37f05159400645d303cc5bcc0ae
oscaresteve <oscarestevea24@gmail.com>
2026-03-11 09:22:30 +0100

    crear componente calidad-panel-component

---
+++ src/app/features/feedback/calidad/ui/calidad-panel-component/calidad-panel-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calidad-panel-component',
+  imports: [],
+  templateUrl: './calidad-panel-component.html',
+  styleUrl: './calidad-panel-component.css',
+})
+export class CalidadPanelComponent {
+
+}
