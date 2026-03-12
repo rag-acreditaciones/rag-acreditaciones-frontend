17769f3ae397dfc598e8f4d2fcb521619519f437
M. <43494720+xsha256@users.noreply.github.com>
2026-03-12 12:22:19 +0100

    Revert "Feedback/rutas (#47)"
    This reverts commit b7c80b11edd1a554c261fb3e1914646424084936.

--- src/app/features/feedback/valoracion/interfaces/valoracion-resumen.ts
+++
@@ -1,8 +0,0 @@
-import { Valoracion } from './valoracion';
-
-export interface ValoracionResumen {
-  totalPositivas: number;
-  totalNegativas: number;
-  ratio: number;
-  detalles: Valoracion[];
-}
